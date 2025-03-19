58974744d9ba8b22aee33ca3767ee327988d058a
ward <wardistitan@gmail.com>
2025-03-19 18:02:03 +0300

    dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "@/components/Hero";
 import SkillsCard from "@/components/SkillsCard";
 import ProjectsCard from "@/components/projectsCard";
 import Contact from "@/components/Contact";
+export const dynamic = "force-dynamic";
 export default function Home() {
   return (
       <>
